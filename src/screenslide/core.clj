88d60dbf30f36662c25b0bb1139dea5963efa014
John Barker <jebarker@gmail.com>
2013-03-16 19:36:23 -0400

    Add some quick functions to get a file listing from a path

--- src/screenslide/core.clj
+++ src/screenslide/core.clj
@@ -2,13 +2,32 @@
   (:use seesaw.core)
   (:gen-class))
 
+(defn present? [val] (not (nil? val)))
+
 (def f (frame :title "Screenslide"))
 (defn takeover-screen []
   (native!)
   (-> f pack! show!)
   (config f :title))
 
+(defn- wildcard-filter
+  "Given a regex, return a FilenameFilter that matches."
+  [regex]
+  (reify java.io.FilenameFilter
+    (accept [_ dir name] (present? (re-find regex name)))))
+
+(defn- directory-list
+  "Given a directory and a regex, return a sorted seq of matching filenames."
+  [dir regex]
+  (sort (.list (clojure.java.io/file dir) (wildcard-filter regex))))
+
+(def regular-file-regex #"[^.]+[.][^.]+")
+
+(defn load-files [path]
+  (directory-list path regular-file-regex))
+
 (defn -main
   "Given a directory, perform a full screen slide-show of all images contained within."
   [& args]
+  (println (load-files (first args)))
   (takeover-screen))
