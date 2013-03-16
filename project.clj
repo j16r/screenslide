b897db8e68c6259e0b8e6b0110cfc1e08924e8ce
John Barker <jebarker@gmail.com>
2013-03-16 19:35:33 -0400

    Add a basic window using seesaw

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Quick and dirty image slideshow"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [seesaw "1.4.2"]]
   :main screenslide.core)
--- src/screenslide/core.clj
+++ src/screenslide/core.clj
@@ -1,7 +1,14 @@
 (ns screenslide.core
+  (:use seesaw.core)
   (:gen-class))
+
+(def f (frame :title "Screenslide"))
+(defn takeover-screen []
+  (native!)
+  (-> f pack! show!)
+  (config f :title))
 
 (defn -main
   "Given a directory, perform a full screen slide-show of all images contained within."
   [& args]
-  (println "Hello, World!"))
+  (takeover-screen))
